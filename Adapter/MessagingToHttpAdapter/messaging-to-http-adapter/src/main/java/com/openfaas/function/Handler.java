4adb9c0f355d1ec8b2159b00c25997b65a5c9fb7
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-09-17 15:35:26 +0200

    Add Messaging-to-http-adapter

--- Adapter/MessagingToHttpAdapter/messaging-to-http-adapter/src/main/java/com/openfaas/function/Handler.java
+++ Adapter/MessagingToHttpAdapter/messaging-to-http-adapter/src/main/java/com/openfaas/function/Handler.java
@@ -1,16 +1,47 @@
 package com.openfaas.function;
 
-import com.openfaas.model.IHandler;
-import com.openfaas.model.IResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openfaas.model.IRequest;
+import com.openfaas.model.IResponse;
 import com.openfaas.model.Response;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
 
 public class Handler implements com.openfaas.model.IHandler {
 
+    private RestTemplate restTemplate = new RestTemplate();
+
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    res.setBody("Hello, world!");
+        String message = req.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode cloudEvent = mapper.readTree(message);
+            System.out.println(cloudEvent);
+            String requestUrl = cloudEvent.get("adapterRequestUrl").asText();
+            String requestMethod = cloudEvent.get("adapterRequestMethod").asText();
+            System.out.println("RequestUrl is:'" + requestUrl + "'");
+            if (!requestUrl.isEmpty()) {
+                String dataElement = cloudEvent.get("data").asText();
+                HttpEntity<String> httpDataEntity = new HttpEntity<>(dataElement);
+                restTemplate.exchange(requestUrl, HttpMethod.resolve(requestMethod), httpDataEntity, String.class);
+                res.setStatusCode(HttpStatus.OK.value());
+            } else {
+                res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            }
+        } catch (Exception e) {
+            res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            res.setBody(e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("Returning status '" + res.getStatusCode() + "'");
+        return res;
+    }
 
-	    return res;
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 }
--- Adapter/MessagingToHttpAdapter/messaging-to-http-adapter/src/test/java/HandlerTest.java
+++ Adapter/MessagingToHttpAdapter/messaging-to-http-adapter/src/test/java/HandlerTest.java
@@ -1,13 +1,76 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 import com.openfaas.function.Handler;
+import com.openfaas.model.IResponse;
+import com.openfaas.model.Request;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.eq;
+
+@RunWith(MockitoJUnitRunner.class)
 public class HandlerTest {
 
-    @Test public void handlerIsNotNull() {
+    @Mock
+    RestTemplate restTemplate;
+
+    @Captor
+    private ArgumentCaptor<String> urlArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<HttpEntity> dataArgumentCaptor;
+
+    @Test
+    public void handlerNoUrl() {
         Handler handler = new Handler();
-        assertTrue("Expected handler not to be null", handler != null);
+        String event = "{\r\n   \"specversion\":\"0.2\",\r\n   \"type\":\"io.github.ust.mico.result\",\r\n   \"source\":\"/router\",\r\n   \"id\":\"A234-1234-1234\",\r\n   \"time\":\"2019-05-08T17:31:00Z\",\r\n   \"contentType\":\"application/json\",\r\n   \"adapterRequestUrl\":\"test\",\r\n   \"data\":\"test\"\r\n}";
+        Request request = new Request(event, null);
+        IResponse response = handler.Handle(request);
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.BAD_REQUEST.value())));
+    }
+
+    @Test
+    public void handlerEmptyMessage() {
+        Handler handler = new Handler();
+        String event = "";
+        Request request = new Request(event, null);
+        IResponse response = handler.Handle(request);
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.BAD_REQUEST.value())));
+    }
+
+    @Test
+    public void handlerCorrect() {
+        Handler handler = new Handler();
+        handler.setRestTemplate(restTemplate);
+        String url = "http://localhost:3000/test";
+        String test = "test";
+        String event = "{\n" +
+                "   \"specversion\":\"0.2\",\n" +
+                "   \"type\":\"io.github.ust.mico.result\",\n" +
+                "   \"source\":\"/router\",\n" +
+                "   \"id\":\"A234-1234-1234\",\n" +
+                "   \"time\":\"2019-05-08T17:31:00Z\",\n" +
+                "   \"contentType\":\"application/json\",\n" +
+                "   \"adapterRequestUrl\":\"" + url + "\",\n" +
+                "   \"adapterRequestMethod\" : \"POST\",\n" +
+                "   \"data\":\"" + test + "\"\n" +
+                "}";
+        Request request = new Request(event, null);
+        IResponse response = handler.Handle(request);
+        Mockito.verify(restTemplate, Mockito.times(1)).exchange(urlArgumentCaptor.capture(), eq(HttpMethod.POST), dataArgumentCaptor.capture(), eq(String.class));
+        assertThat(urlArgumentCaptor.getValue(), is(equalTo(url)));
+        assertThat(dataArgumentCaptor.getValue().getBody(), is(equalTo(test)));
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatus.OK.value())));
     }
 
 }
